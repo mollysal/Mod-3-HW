b3faac4e915012fc4f1e9285917bdbe858bcb2b7
Molly Salmonsen <mollysalmonsen@Mollys-MacBook-Pro-2.local>
2022-07-19 21:35:33 -0400

    added the rest of the letters to arrays

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ function writePassword() {
 //Declare function of generatePassword
 function generatePassword() {
   //variables holding the different characters
-  var upppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'];
-  var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'];
+  var upppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var specialCharaacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
   var numericCharacters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
